c2b30196c9520f4460eb541855f9068b17449560
MarkRullan <164309766+MarkRullan@users.noreply.github.com>
2024-11-04 16:22:24 +0800

    Create OE #7.py

---
+++ OE #7.py
@@ -0,0 +1,20 @@
+#OE 7
+#Mark Rullan
+class TekkenCharacter:
+    def __init__(self, name, ability):
+        self.name = name
+        self.ability = ability
+        
+    def introduce(self, func):
+        def wrapper():
+            print("introducing...")
+            func()
+            print("This character is amazing!")
+        return wrapper
+        
+character = TekkenCharacter("Raven", "Shadow Clones")
+
+@character.introduce
+def character_intro():
+    print(f"I am {character.name} and I can use {character.ability}.")
+character_intro()
